96171e51ceed01689b9d801a6db75c1a868c3434
Michael Maio <michael.maio22@gmail.com>
2017-03-06 11:36:53 -0800

    render turntable in app

--- js/components/app.component.js
+++ js/components/app.component.js
@@ -4,9 +4,10 @@ import { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Paper from 'material-ui/Paper';
 
-
+import Turntable from './turntable.component';
 import Header from './header.component';
 
+// import Turntable2 from './turntable2';
 
 const styles = {
   tt_mixer: {
@@ -27,6 +28,11 @@ export default class App extends Component {
         <MuiThemeProvider>
           <Header />
         </MuiThemeProvider>
+        </div>
+        <div className="row">
+        <div className="container-fluid tt-mixer-container col-lg-6 col-lg-offset-0 col-md-6 col-md-offset-3 col-sm-10 col-sm-offset-1 col-xs-12 col-xs-offset-0" style={styles.tt_mixer}>
+          <Turntable />
+        </div>
         </div>
       </div>
     );
