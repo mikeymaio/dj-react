4724968381c3aa761e1e603bbf319e00cca71066
Michael Maio <michael.maio22@gmail.com>
2017-03-07 13:18:04 -0800

    add deckNum argument to actions

--- js/actions/index.action.js
+++ js/actions/index.action.js
@@ -1,88 +1,91 @@
-export function selectSong(song) {
+export function selectSong(song, deckNum) {
     //const SONG_SELECTED = 'SONG_SELECTED'
+    const SONG_SELECTED = 'SONG_SELECTED';
     console.log(song.artist.song, ' has been selected');
     return {
-        type: 'SONG_SELECTED',
+        type: SONG_SELECTED + deckNum,
         payload: song
     }
 }
 
-export function startStopSong(play) {
+export function startStopSong(deckNum) {
     console.log('play/pause clicked');
+    const START_STOP_SONG = 'START_STOP_SONG';
     return {
-        type: 'START_STOP_SONG',
-        payload: play
+        type: START_STOP_SONG + deckNum,
     };
 }
 
-export function handleVolumeChange(value) {
+export function handleVolumeChange(value, deckNum) {
     console.log('volume changed');
+    const UPDATE_VOLUME = 'UPDATE_VOLUME';
     return {
-        type: 'UPDATE_VOLUME',
+        type: UPDATE_VOLUME + deckNum,
         payload: value
     }
 }
 
-export function handlePlaybackSpeed(value) {
+export function handlePlaybackSpeed(value, deckNum) {
     console.log('playback speed changed');
+    const UPDATE_PLAYBACK_SPEED = 'UPDATE_PLAYBACK_SPEED';
     return {
-        type: 'UPDATE_PLAYBACK_SPEED',
+        type: UPDATE_PLAYBACK_SPEED + deckNum,
         payload: value
     }
 }
 
-export function handleTrebleControl(value) {
+export function handleTrebleControl(value, deckNum) {
     console.log('treble value changed');
     return {
-        type: 'UPDATE_TREBLE_CONTROL',
+        type: 'UPDATE_TREBLE_CONTROL' + deckNum,
         payload: value
     }
 }
 
-export function handleMidControl(value) {
+export function handleMidControl(value, deckNum) {
     console.log('mid value changed');
     return {
-        type: 'UPDATE_MID_CONTROL',
+        type: 'UPDATE_MID_CONTROL' + deckNum,
         payload: value
     }
 }
 
-export function handleBassControl(value) {
+export function handleBassControl(value, deckNum) {
     console.log('bass value changed');
     return {
-        type: 'UPDATE_BASS_CONTROL',
+        type: 'UPDATE_BASS_CONTROL' + deckNum,
         payload: value
     }
 }
 
-export function handleFilterChange(value) {
+export function handleFilterChange(value, deckNum) {
     console.log('filter cutoff value changed');
     return {
-        type: 'UPDATE_FILTER',
+        type: 'UPDATE_FILTER' + deckNum,
         payload: value
     }
 }
 
-export function handleReverbChange(value) {
+export function handleReverbChange(value, deckNum) {
     console.log('reverb mix value changed');
     return {
-        type: 'UPDATE_REVERB',
+        type: 'UPDATE_REVERB' + deckNum,
         payload: value
     }
 }
 
-export function handleDelayChange(value) {
+export function handleDelayChange(value, deckNum) {
     console.log('delay mix value changed');
     return {
-        type: 'UPDATE_DELAY',
+        type: 'UPDATE_DELAY' + deckNum,
         payload: value
     }
 }
 
-export function handleDistortionChange(value) {
+export function handleDistortionChange(value, deckNum) {
     console.log('distortion value changed');
     return {
-        type: 'UPDATE_DISTORTION',
+        type: 'UPDATE_DISTORTION' + deckNum,
         payload: value
     }
 }
