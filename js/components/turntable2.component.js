b78b3d541062c0c9ef14af291840c85dd382dce1
HypeUFO <michael.maio22@gmail.com>
2017-03-29 09:12:57 -0700

    update path to file upload

--- js/components/turntable2.component.js
+++ js/components/turntable2.component.js
@@ -190,7 +190,7 @@ class Turntable extends React.Component {
 
     const upload = {
 
-          url: '../dj-react/' + file,
+          url: file,
           name: acceptedFiles[0].name,
           title: acceptedFiles[0].name,
           cover: "../dj-react/assets/images/djR-vinyl-label.jpg",
