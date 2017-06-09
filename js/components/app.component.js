bbbc671907e11278953e81477bf9f53c6911bb9d
HypeUFO <michael.maio22@gmail.com>
2017-06-09 11:16:35 -0700

    add alert for older browsers (web audio compatibility)

--- js/components/app.component.js
+++ js/components/app.component.js
@@ -11,18 +11,26 @@ class App extends Component {
     console.log("Hello fellow dev! Why not fork the repo and send me a PR? There is much work to be done! https://github.com/HypeUFO/dj-react")
   }
   render() {
-    const AudioContext = window.AudioContext || window.webkitAudioContext;
-    const audioContext = new AudioContext();
-    return (
-      <div>
-        <div className="row">
-          <Header />
+    const AudioContext = window.AudioContext || window.webkitAudioContext || false;
+
+    if (AudioContext) {
+      // Create an audio context
+      const audioContext = new AudioContext();
+
+      return (
+        <div>
+          <div className="row">
+            <Header />
+          </div>
+          <IntroModal />
+          <Table audioContext={audioContext} />
         </div>
-        {/*<NewMeter audioContext={audioContext} />*/}
-        <IntroModal />
-        <Table audioContext={audioContext} />
-     </div>
-    );
+      );
+    } else {
+      // Web Audio API is not supported
+      // Alert the user
+      alert("Sorry, but the Web Audio API is not supported by your browser. Please, consider upgrading to the latest version or downloading Google Chrome or Mozilla Firefox");
+    }
   }
 }
 
